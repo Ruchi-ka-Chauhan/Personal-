a200915ce99180d30106db56b6d286b5dee8f1f2
RuchikaChauhan <ruchika23chauhan@gmail.com>
2026-06-13 23:15:31 +0530

    change in file name

---
+++ prev.py
@@ -0,0 +1,13 @@
+
+def get_choices():
+ player_choice = input("Enter a choice (rock,paper,sciccors:")
+ computer_choice="paper"
+ choices = {"player":player_choice ,"computer": computer_choice}
+ return choices
+ #functio is a set of code which only run when it is called
+ return player_choice
+
+choices = get_choices()
+print(choices)   
+#dictionary is used to store value in key value pair
+dict={"name": "baue" , "color":choices}
